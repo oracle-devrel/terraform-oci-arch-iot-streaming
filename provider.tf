8bfb7ae4ec82dc3e27c72716174f06a4a97230c9
Phil Wilkins <phil@mp3monster.org>
2022-07-21 20:37:35 +0100

    update provider to include terraform declaration

--- provider.tf
+++ provider.tf
@@ -1,6 +1,13 @@
 ## Copyright (c) 2021, Oracle and/or its affiliates.
 ## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
-
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+    }
+  }
+}
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
@@ -17,4 +24,15 @@ provider "oci" {
   private_key_path     = var.private_key_path
   region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
   disable_auto_retries = "true"
+}
+
+
+provider "oci" {
+  alias        = "current_region"
+  tenancy_ocid = var.tenancy_ocid
+  region       = var.region
+
+  #  user_ocid        = var.user_ocid
+  #  fingerprint      = var.fingerprint
+  #  private_key_path = var.private_key_path
 }
